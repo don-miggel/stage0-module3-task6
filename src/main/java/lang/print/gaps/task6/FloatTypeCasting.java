95b9bf0ad58107e0c8d3921bc435ccf3221cc58b
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-18 19:00:41 +0200

    Module 3, task 6 done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        int roundedVal = Math.round(numberToBeRounded);
+        System.out.println(roundedVal);
+
+
+
+
     }
 }
